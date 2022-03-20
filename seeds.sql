2866448726cc1a05f3867e3754d2feeb252bd46d
Prathibha <pratibha.indel@gmail.com>
2022-03-19 21:36:18 -0400

    corrected file

--- seeds.sql
+++ seeds.sql
@@ -1,14 +1,14 @@
---department table--
+USE employee_DB;
 INSERT INTO department (name)
 VALUES ("engineering"),
         ("finance"),
         ("legal"),
-        ("sales")
+        ("sales");
 
 
- SELECT * FROM DEPARTMENT;   
+ SELECT * FROM department;   
 
---role table--
+
  INSERT INTO role (title, salary, department_id) VALUES ('Staff', 20000, 1);
 INSERT INTO role (title, salary, department_id) VALUES ('Health ', 50000, 1);
 INSERT INTO role (title, salary, department_id) VALUES ('Accountant', 80000, 1);
@@ -20,14 +20,14 @@ INSERT INTO role (title, salary, department_id) VALUES ('Jr. Engineer', 60000, 2
 INSERT INTO role (title, salary, department_id) VALUES ('Engineering Intern', 45000, 2);
 INSERT INTO role (title, salary, department_id) VALUES ('Lead Engineer', 110000, 2);
 
-SELECT * FROM ROLE;
+SELECT * FROM role;
 
---employee table--
+
  INSERT INTO employee(first_name, last_name, role_id) VALUES ('Tele', 'Deon', 1);
 INSERT INTO employee(first_name, last_name, role_id) VALUES ('Kemph', 'Shore', 2);
 INSERT INTO employee(first_name, last_name, role_id) VALUES ('Swastic', 'Deo', 3);
-INSERT INTO employee(first_name, last_name, role_id, manager_id) VALUES ('Pradi', 'Jain', 4, 1);
-INSERT INTO employee(first_name, last_name, role_id, manager_id) VALUES ('Deon', 'Candy', 5, 2);
+INSERT INTO employee(first_name, last_name, role_id, manager_id) VALUES ('Pradi', 'Jain', 3, null);
+INSERT INTO employee(first_name, last_name, role_id, manager_id) VALUES ('Deon', 'Candy', 1, 2);
 INSERT INTO employee(first_name, last_name, role_id, manager_id) VALUES ('Don', 'sear', 6, 3);
 INSERT INTO employee(first_name, last_name, role_id, manager_id) VALUES ('Shore', 'Ana', 7, 4);
 INSERT INTO employee(first_name, last_name, role_id, manager_id) VALUES ('Richard', 'Bob', 8, 5);
